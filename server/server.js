b123c37ce9290ce9c3f4a438acd29771250112a9
madhushanka Deshan <madhushankadeshan96@gmail.com>
2018-04-23 13:30:01 +0530

    Add form for messages and show incoming messages in browser

--- server/server.js
+++ server/server.js
@@ -15,25 +15,14 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('NEw user connected');
 
-  socket.emit('newMessage', {
-    from: 'Admin',
-    text: 'Welcome to the chat app',
-    createdAt: new Date().getTime()
-  });
+  socket.emit('newMessage', generateMessage('admin','Welcome to the chat app'));
 
-  socket.broadcast.emit('newMessage', {
-    from: 'admin',
-    text: 'new user joined',
-    createdAt: new Date().getTime()
-  });
+  socket.broadcast.emit('newMessage', generateMessage('admin','new user joined'));
 
-  socket.on('createMessage', (message) => {
+  socket.on('createMessage', (message,callback) => {
     console.log('createMessage',message);
-    io.emit('newMessage', {
-      from: message.from,
-      text: message.text,
-      createAt: new Date().getTime()
-    });
+    io.emit('newMessage', generateMessage(message.from, message.text));
+    callback('This is from the server.');
     // socket.broadcast.emit('newMessage', {
     //   from:message.from,
     //   text:message.text,
